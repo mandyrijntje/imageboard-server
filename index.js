3a63456f30d220aed916730b4434574f58e6d648
messmonte <amandamariamonte@gmail.com>
2020-03-02 11:11:03 +0100

    added db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./db");
 // const Post = require("./post/model"); //dont need to import db because model imports db!
 // const postRouter = require("./post/router");
 const app = express();
