888a22fe69afdf96c5ff97a175bc4a7243efec97
messmonte <amandamariamonte@gmail.com>
2020-03-02 11:06:31 +0100

    changes

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
+// const Sequelize = require("sequelize");
+// const databaseUrl =
+//   process.env.DATABASE_URL ||
+//   "postgres://postgres:secret@localhost:5432/postgres";
+// const db = new Sequelize(databaseUrl);
+// //
+// db.sync()
+//   .then(() => console.log("Database connected"))
+//   .catch(err => console.error(err));
+
 // module.exports = db;
