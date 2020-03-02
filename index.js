14c92b2e7cae2bbe725de925795aad356d8b184b
messmonte <amandamariamonte@gmail.com>
2020-03-02 10:57:23 +0100

    changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const Post = require("./post/model"); //dont need to import db because model imports db!
-const postRouter = require("./post/router");
+// const Post = require("./post/model"); //dont need to import db because model imports db!
+// const postRouter = require("./post/router");
 const app = express();
 const port = process.env.PORT || 4000;
 
 const parser = express.json(); //dont need to install body parser.
 app.use(parser); //parser needs to be before ROUTER!
 
-app.use(postRouter);
+// app.use(postRouter);
 app.get("/data", (request, response) => {
   response.send("SOME DAta");
 });
