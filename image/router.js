f0c575ac8c7a1396cf56b729a0ec76ac295b24fd
messmonte <amandamariamonte@gmail.com>
2020-03-03 10:03:14 +0100

    minor changes in router

--- image/router.js
+++ image/router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const auth = require("../auth/middleware");
 const Image = require("./model");
 const router = express.Router(); //getting router from express and assigning to var
 
@@ -8,7 +9,7 @@ router.get("/images", (req, res, next) => {
     .catch(next);
 });
 
-router.post("/images", (req, res, next) => {
+router.post("/images", auth, (req, res, next) => {
   console.log(req.body);
   Image.create(req.body)
     .then(image => {
@@ -17,5 +18,11 @@ router.post("/images", (req, res, next) => {
 
     .catch(next);
 });
+
+router.delete("/images/:id", (req, res, next) =>
+  Image.destroy({ where: { id: req.params.id } })
+    .then(number => res.send({ number }))
+    .catch(next)
+);
 
 module.exports = router;
